e9d697191956e305f43165295edd6ce4d47b5a62
IotaSphere <40618095+Geocube101@users.noreply.github.com>
2024-11-06 13:54:32 -0500

    Added Text Counters

--- Coliseum of the Gods/Assets/Scripts/Session.cs
+++ Coliseum of the Gods/Assets/Scripts/Session.cs
@@ -5,17 +5,23 @@ using System.Linq;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UI;
 
 namespace GC.IotaScripts
 {
 	public class MySessionComponent : MonoBehaviour
 	{
+		internal static Camera MainCamera { get; private set; }
+
 		private DateTime LastArenaShiftTime = DateTime.MinValue;
 		private DateTime LastWeaponShiftTime = DateTime.MinValue;
-		private GameObject TextCounter;
+
+		private GameObject[] PlayerTextTL = new GameObject[2];
 
 		internal MyFloorShifter FloorShifter { get; private set; }
-		internal TMP_Text TextCounterText { get; private set; }
+
+		internal double RemainingDeathmatchTimeSeconds;
 
 		public uint FloorSubdivisionLevel = 10;
 		public float FloorSwitchTimeSeconds = 30;
@@ -23,7 +29,13 @@ namespace GC.IotaScripts
 		public float MaxTileHeightOffset = 0;
 		public double DeathmatchTimeSeconds = 120;
 
-		internal double RemainingDeathmatchTimeSeconds;
+		internal static string TimeSecondsToHHMMSS(double time_seconds)
+		{
+			uint hours = (uint) time_seconds / 3600;
+			uint minutes = (uint) time_seconds / 60;
+			uint seconds = (uint) time_seconds % 60;
+			return $"{hours:D2}:{minutes:D2}:{seconds:D2}";
+		}
 
 		private void Awake()
 		{
@@ -35,12 +47,46 @@ namespace GC.IotaScripts
 		{
 			this.FloorShifter = GameObject.Find("Floor").GetComponent<MyFloorShifter>();
 			this.FloorShifter.Initialize(this.FloorSubdivisionLevel);
-			this.TextCounter = GameObject.Find("CounterText");
-			this.TextCounterText = this.TextCounter.GetComponent<TMP_Text>();
 			this.RemainingDeathmatchTimeSeconds = this.DeathmatchTimeSeconds;
 
-			this.ShiftUpdateArena();
+			GameObject camera = GameObject.Find("MainCamera");
+			MySessionComponent.MainCamera = camera.GetComponent<Camera>();
+
+			for (int i = 0; i < this.PlayerTextTL.Length; ++i)
+			{
+				GameObject text = new GameObject($"Text{i}");
+
+				MeshRenderer renderer = text.AddComponent<MeshRenderer>();
+				renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+				renderer.receiveShadows = false;
+				renderer.receiveGI = 0;
+				renderer.lightProbeUsage = UnityEngine.Rendering.LightProbeUsage.BlendProbes;
+				renderer.reflectionProbeUsage = UnityEngine.Rendering.ReflectionProbeUsage.BlendProbes;
+				renderer.probeAnchor = null;
+				renderer.motionVectorGenerationMode = MotionVectorGenerationMode.Object;
+				renderer.allowOcclusionWhenDynamic = true;
+
+				TextMeshPro text_comp = text.AddComponent<TextMeshPro>();
+				text_comp.text = "";
+				text_comp.fontSize = 18;
+				text_comp.alignment = TextAlignmentOptions.Left;
+				text_comp.enableWordWrapping = false;
+				text_comp.overflowMode = TextOverflowModes.Ellipsis;
+				text_comp.color = Color.white;
+				text_comp.outlineColor = Color.black;
+				text_comp.outlineWidth = 0.25f;
+				text_comp.fontMaterial.SetColor("_OutlineColor", Color.black);
+				//text_comp.fontMaterial.SetFloat("_OutlineWidth", 0.25f);
+				text_comp.autoSizeTextContainer = true;
+
+				renderer.materials = new Material[1] { text_comp.fontMaterial };
+				text.transform.SetParent(camera.transform);
+				text.transform.localScale = new Vector3(0.025f, 0.025f, 0.025f);
+				this.PlayerTextTL[i] = text;
+			}
+
 			this.ShiftRandomizeWeapons();
+			this.LastArenaShiftTime = DateTime.UtcNow;
 		}
 
 		private void OnDisable()
@@ -50,17 +96,41 @@ namespace GC.IotaScripts
 
 		private void FixedUpdate()
 		{
+			// Shift/randomize session things
 			DateTime now = DateTime.UtcNow;
 			if ((now - this.LastArenaShiftTime).TotalSeconds > this.FloorSwitchTimeSeconds) this.ShiftUpdateArena();
 			if ((now - this.LastWeaponShiftTime).TotalSeconds > this.WeaponSwitchTimeSeconds) this.ShiftRandomizeWeapons();
+
+			// Update text headers
 			this.RemainingDeathmatchTimeSeconds -= Time.deltaTime;
-			
-			if (this.RemainingDeathmatchTimeSeconds >= 0) this.TextCounterText.text = $"{(uint) (this.RemainingDeathmatchTimeSeconds / 60d):D2}:{(uint) (this.RemainingDeathmatchTimeSeconds % 60d):D2}";
-			else
+			if (this.RemainingDeathmatchTimeSeconds >= 0) this.SetHeader(0, $"Deathmatch In: {MySessionComponent.TimeSecondsToHHMMSS(this.RemainingDeathmatchTimeSeconds)}");
+			else this.SetHeader(0, $"Deathmatch Time: {MySessionComponent.TimeSecondsToHHMMSS(-this.RemainingDeathmatchTimeSeconds)}", Color.red);
+			this.SetHeader(1, $"Arena Shift In: {MySessionComponent.TimeSecondsToHHMMSS(this.RemainingDeathmatchTimeSeconds % 30)}");
+
+			// Update text
+			float by = MySessionComponent.MainCamera.pixelHeight - 25;
+
+			for (int i = 0; i < this.PlayerTextTL.Length; ++i)
 			{
-				double deathmatch_time = -this.RemainingDeathmatchTimeSeconds;
-				this.TextCounterText.text = $"{(uint) (deathmatch_time / 60d):D2}:{(uint) (deathmatch_time % 60d):D2}";
-				this.TextCounterText.color = Color.red;
+				GameObject text = this.PlayerTextTL[i];
+				TextMeshPro tmp = text?.GetComponent<TextMeshPro>();
+				if ((tmp?.text.Length ?? 0) == 0) continue;
+
+				Vector2 sprite_size = new Vector2(tmp.renderedWidth, tmp.renderedHeight);
+				Vector2 local_sprite_size = sprite_size / tmp.pixelsPerUnit;
+				Vector3 world_size = local_sprite_size;
+				world_size.x *= text.transform.lossyScale.x;
+				world_size.y *= text.transform.lossyScale.y;
+
+				//convert to screen space size
+				Vector3 screen_size = 0.5f * world_size / MySessionComponent.MainCamera.orthographicSize;
+				screen_size.y *= MySessionComponent.MainCamera.aspect;
+
+				//size in pixels
+				Vector2 in_pixels = new Vector2(screen_size.x * MySessionComponent.MainCamera.pixelWidth, screen_size.y * MySessionComponent.MainCamera.pixelHeight) * 0.5f;
+				text.transform.position = MySessionComponent.MainCamera.ScreenToWorldPoint(new Vector3(in_pixels.x * 6f, by, 1));
+				text.transform.rotation = MySessionComponent.MainCamera.transform.rotation;
+				by -= in_pixels.y + 20;
 			}
 		}
 
@@ -80,6 +150,16 @@ namespace GC.IotaScripts
 			}
 
 			this.LastWeaponShiftTime = DateTime.UtcNow;
+		}
+
+		internal void SetHeader(int index, string header, Color? color = null)
+		{
+			if (index > this.PlayerTextTL.Length) throw new InvalidOperationException("The specified header index is out of bounds");
+			GameObject text = this.PlayerTextTL[index];
+			if (text == null) return;
+			TextMeshPro tmp = text.GetComponent<TextMeshPro>();
+			tmp.text = header ?? "";
+			tmp.color = color ?? tmp.color;
 		}
 	}
 }
